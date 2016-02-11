45cd0cecbba0b09c8f88b7dace692de1744d2c2a
Ailuridaes <ailuridaes@gmail.com>
2016-02-10 19:16:18 -0800

    defaults to only display tasks of current user

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,51 +11,52 @@ import views.html.*;
 
 public class Application extends Controller {
 
-	@Security.Authenticated(Secured.class)
-    public Result index() {
-        return ok(index.render(
-			Project.find.all(),
-            Task.find.all()
-		));
-    }
-	
-	public Result login() {
-        return ok(
-            login.render(form(Login.class))
-        );
-    }
-	
-	public Result authenticate() {
-		Form<Login> loginForm = form(Login.class).bindFromRequest();
-		if (loginForm.hasErrors()) {
-			return badRequest(login.render(loginForm));
-		} else {
-			session().clear();
-			session("email", loginForm.get().email);
-			return redirect(
-				routes.Application.index()
-			);
+		@Security.Authenticated(Secured.class)
+		public Result index() {
+				return ok(index.render(
+						Project.findInvolving(request().username()),
+						Task.findTodoInvolving(request().username()),
+						User.find.byId(request().username())
+				));
 		}
-	}
-	
-	public Result logout() {
-		session().clear();
-		flash("success", "You've been logged out");
-		return redirect(
-			routes.Application.login()
-		);
-	}
-		
-	public static class Login {
-		public String email;
-		public String password;
-		
-		public String validate() {
-			if (User.authenticate(email, password) == null) {
-			  return "Invalid username or password";
+
+		public Result login() {
+	      return ok(
+	          login.render(form(Login.class))
+	      );
+	  }
+
+		public Result authenticate() {
+				Form<Login> loginForm = form(Login.class).bindFromRequest();
+				if (loginForm.hasErrors()) {
+						return badRequest(login.render(loginForm));
+				} else {
+						session().clear();
+						session("email", loginForm.get().email);
+						return redirect(
+								routes.Application.index()
+						);
 			}
-			return null;
 		}
-	}
+
+		public Result logout() {
+				session().clear();
+				flash("success", "You've been logged out");
+				return redirect(
+						routes.Application.login()
+				);
+		}
+
+		public static class Login {
+			  public String email;
+				public String password;
+
+				public String validate() {
+						if (User.authenticate(email, password) == null) {
+						  	return "Invalid username or password";
+						}
+						return null;
+				}
+		}
 
 }
